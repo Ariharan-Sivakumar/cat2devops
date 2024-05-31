d5e8ccae073e803be52594ad327e98ec2d547bb5
unixlab <unixlab@unixlab-Vostro-3902>
2024-05-31 14:54:04 +0530

    First commit for cat2devops

---
+++ cat2devops/src/main/java/cat2devops/cat2devops/App.java
@@ -0,0 +1,13 @@
+package cat2devops.cat2devops;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
